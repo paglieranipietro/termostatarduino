e3ee767576de965c58833178bbbc1c1b3993a0d5
Freddi <77694522@itiscastelli.onmicrosoft.com>
2025-01-22 13:47:26 +0100

    Rimossa dipendenza da raylib e aggiunto supporto JSON

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import multiprocessing as mp
 import serial as sr
-from pyray import *
+import json
 
 data = 0
 
 def serial_task(q: mp.Queue):
     arduino = sr.Serial("COM6", 9600)
     while True:
-        n = arduino.readline()
-        q.put(n)
+        line = arduino.readline().strip()
+        line = line.decode("utf-8")
+        msg = json.load(line)
+        q.put(msg)
 
 def app():
     global data
@@ -17,19 +19,27 @@ def app():
     max_gradi = 30
     max_y = 50
     x = 170
-    init_window(800, 450, "Hello")
-    while not window_should_close():
-        if not q.empty():
-            data = int(q.get())
-        begin_drawing()
-        clear_background(WHITE)
-        draw_text(f"data = {data}", 190, 200, 20, VIOLET)
-        draw_line(x, min_y, x, max_y, RED)
-        y = int( ((data - min_gradi) * (max_y - min_y)) / (max_gradi - min_gradi) )
-        draw_circle(x, y + min_y, 10, RED)
-        end_drawing()
 
-    close_window()
+
+    # init_window(800, 450, "Hello")
+    # while not window_should_close():
+        #if not q.empty():
+            #data = q.get()
+        #begin_drawing()
+
+        #clear_background(WHITE)
+
+
+        # temp = data["temp"]
+        # draw_text(f"data = {temp}", 190, 200, 20, VIOLET)
+        # draw_line(x, min_y, x, max_y, RED)
+
+        # y = int( ((data - min_gradi) * (max_y - min_y)) / (max_gradi - min_gradi) )
+        # draw_circle(x, y + min_y, 10, RED)
+
+        #end_drawing()
+
+    #close_window()
 
 if __name__ == "__main__":
     mp.freeze_support()
