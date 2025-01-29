eba0899dd95f65408bb3b3646803a7c46a41872c
Freddi <77694522@itiscastelli.onmicrosoft.com>
2025-01-29 13:24:09 +0100

    Aggiunta comunicazione Python-Arduino

--- main.py
+++ main.py
@@ -2,44 +2,22 @@ import multiprocessing as mp
 import serial as sr
 import json
 
-data = 0
-
 def serial_task(q: mp.Queue):
-    arduino = sr.Serial("COM6", 9600)
+    arduino = sr.Serial("COM8", 9600)
     while True:
-        line = arduino.readline().strip()
+        line = arduino.readline()
         line = line.decode("utf-8")
-        msg = json.load(line)
+        msg = {}
+        msg = json.loads(line)
         q.put(msg)
 
 def app():
-    global data
-    min_gradi = 20
-    min_y = 400
-    max_gradi = 30
-    max_y = 50
-    x = 170
+    while True:
+        data = {}
+        if not q.empty():
+            data = q.get()
+            print(data)
 
-
-    # init_window(800, 450, "Hello")
-    # while not window_should_close():
-        #if not q.empty():
-            #data = q.get()
-        #begin_drawing()
-
-        #clear_background(WHITE)
-
-
-        # temp = data["temp"]
-        # draw_text(f"data = {temp}", 190, 200, 20, VIOLET)
-        # draw_line(x, min_y, x, max_y, RED)
-
-        # y = int( ((data - min_gradi) * (max_y - min_y)) / (max_gradi - min_gradi) )
-        # draw_circle(x, y + min_y, 10, RED)
-
-        #end_drawing()
-
-    #close_window()
 
 if __name__ == "__main__":
     mp.freeze_support()
