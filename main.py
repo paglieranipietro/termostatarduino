c10a20ca629ea86932f9f821f7075ee0a8c4f377
Pietro <paglieranipietro@gmail.com>
2025-02-05 13:22:45 +0100

    testing

--- main.py
+++ main.py
@@ -1,28 +1,56 @@
 import multiprocessing as mp
-import serial as sr
+import serial as ser
 import json
+import dearpygui.dearpygui as dpg
+import threading
+import time
 
-def serial_task(q: mp.Queue, port: str):
-    arduino = sr.Serial(port, 9600)
+def serial_task(q: mp.Queue):
+    arduino = ser.Serial("COM5", 9600)
     while True:
         line = arduino.readline()
         line = line.decode("utf-8")
-        msg = {}
         msg = json.loads(line)
         q.put(msg)
 
-def app():
-    while True:
-        data = {}
+# creating data
+xs = [i * 0.001 for i in range(1001)]
+ys = [0] * 1001
+ys1 = []
+ys2 = []
+
+def update_data():
+    while dpg.is_dearpygui_running():
         if not q.empty():
             data = q.get()
-            print(data)
-
+            temperature = float(data["temp"])
+            ys.append(temperature)
+            ys.pop(0)
+            dpg.set_value("temperature_series", [xs, ys])
+        time.sleep(1)  # Update every second
 
 if __name__ == "__main__":
-    port = "COM" + str(input("Enter serial port number: "))
     mp.freeze_support()
-    q = mp.Queue() # creo la coda
-    p = mp.Process(target=serial_task,args=(q, port))
+    q = mp.Queue()  # create the queue
+    p = mp.Process(target=serial_task, args=(q,))
     p.start()
-    app()
+
+    with dpg.window(label="Tutorial"):
+        # create plot
+        with dpg.plot(label="Shaded Plot", tag="shaded_plot_1", width=800, height=500):
+            dpg.add_plot_axis(dpg.mvXAxis, label="x")
+            with dpg.plot_axis(dpg.mvYAxis, label="y"):
+                dpg.add_shade_series(xs, ys1, y2=ys2, label="Shaded Area", fill_color=(0, 0, 255, 100))
+                dpg.add_line_series(xs, ys, label="Function", color=(0, 0, 255, 255), tag="temperature_series")
+
+    dpg.create_viewport(title='Custom Title', width=1400, height=600)
+    dpg.setup_dearpygui()
+    dpg.show_viewport()
+
+    # Start the update thread
+    update_thread = threading.Thread(target=update_data)
+    update_thread.start()
+
+    dpg.start_dearpygui()
+    dpg.destroy_context()
+    p.join()
