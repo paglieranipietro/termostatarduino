b48bdb5697726065acc6118b4f8ccfada24236a1
Freddi <77694522@itiscastelli.onmicrosoft.com>
2025-02-05 12:02:55 +0100

    Aggiunta richiesta del numero di porta all'accensione del programma tramite console

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import multiprocessing as mp
 import serial as sr
 import json
 
-def serial_task(q: mp.Queue):
-    arduino = sr.Serial("COM8", 9600)
+def serial_task(q: mp.Queue, port: str):
+    arduino = sr.Serial(port, 9600)
     while True:
         line = arduino.readline()
         line = line.decode("utf-8")
@@ -20,8 +20,9 @@ def app():
 
 
 if __name__ == "__main__":
+    port = "COM" + str(input("Enter serial port number: "))
     mp.freeze_support()
     q = mp.Queue() # creo la coda
-    p = mp.Process(target=serial_task,args=(q,))
+    p = mp.Process(target=serial_task,args=(q, port))
     p.start()
     app()
