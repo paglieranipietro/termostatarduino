2ae6700f8de775824834dfa4576dc1dea99b9346
pietr <paglieranipietro@gmail.com>
2025-02-13 18:38:40 +0100

    test

--- main.py
+++ main.py
@@ -1,56 +1,166 @@
+import dearpygui.dearpygui as dpg
 import multiprocessing as mp
+import random
 import serial as ser
 import json
-import dearpygui.dearpygui as dpg
-import threading
-import time
+import datetime
+from screeninfo import get_monitors
 
-def serial_task(q: mp.Queue):
-    arduino = ser.Serial("COM5", 9600)
+dpg.create_context()
+
+# creating data
+xs = [0, ] * 20
+ys_temp = []
+ys_humid = []
+ys1_temp = []
+ys2_temp = []
+ys1_humid = []
+ys2_humid = []
+MAX_WIDTH = get_monitors()[0].width
+MAX_HEIGHT = get_monitors()[0].height
+
+
+def update_xs(value):
+    if xs[len(xs) - 1] == 0:
+        for i in range(len(xs)):
+            if xs[i] != 0:
+                xs[i] = value
+    else:
+        xs.pop(0)
+        xs.append(value)
+        ys_temp.pop(0)
+        ys_humid.pop(0)
+        ys1_temp.pop(0)
+        ys2_temp.pop(0)
+        ys1_humid.pop(0)
+        ys2_humid.pop(0)
+
+
+def salva_dati(temp, umid, filename="dati_temperatura.json"):
+    nuovo_dato = {
+        "data": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "temp": temp,
+        "umid": umid
+    }
+    try:
+        with open(filename, "a") as file:
+            file.write(json.dumps(nuovo_dato) + "\n")
+    except Exception as e:
+        print(f"Errore durante il salvataggio dei dati: {e}")
+
+
+def update_zoom():
+    if xs:
+        min_x = 0
+        max_x = 20
+        dpg.set_axis_limits("x_axis_temp", min_x, max_x)
+        dpg.set_axis_limits("x_axis_humid", min_x, max_x)
+
+    if ys_temp and ys_humid:
+        min_y_temp = min(min(ys1_temp), min(ys2_temp))
+        max_y_temp = max(max(ys1_temp), max(ys2_temp))
+        min_y_humid = min(min(ys1_humid), min(ys2_humid))
+        max_y_humid = max(max(ys1_humid), max(ys2_humid))
+        dpg.set_axis_limits("y_axis_temp", min_y_temp - 10, max_y_temp + 10)
+        dpg.set_axis_limits("y_axis_humid", min_y_humid - 10, max_y_humid + 10)
+
+
+def update_data_callback():
+    if not q.empty():
+        line = q.get()
+        if line:
+            update_xs(float(datetime.now().timestamp()))
+
+            temperature = float(line["temp"])
+            humidity = float(line["umid"])
+            ys_temp.append(temperature)
+            ys_humid.append(humidity)
+
+            if len(xs) < 20:
+                xs.append(len(ys_temp) - 1)
+
+            dpg.set_value("temperature_series", [xs, ys_temp])
+            dpg.set_value("humidity_series", [xs, ys_humid])
+
+            ys1_temp.append(temperature + random.uniform(0.3, 0.5))
+            ys2_temp.append(temperature - random.uniform(0.3, 0.5))
+            ys1_humid.append(humidity + random.uniform(0.3, 0.5))
+            ys2_humid.append(humidity - random.uniform(0.3, 0.5))
+
+            salva_dati(temperature, humidity)
+            update_zoom()
+
+            # Aggiornamento LED
+
+            led1 = bool(line["led1"])
+            led2 = bool(line["led2"])
+
+            if led1:
+                dpg.configure_item("led1", fill=(0, 255, 0))
+            else:
+                dpg.configure_item("led1", fill=(0, 30, 0))
+
+            if led2:
+                dpg.configure_item("led2", fill=(255, 0, 0))
+            else:
+                dpg.configure_item("led2", fill=(30, 0, 0))
+
+
+def serial_task(q: mp.Queue, com):
+    try:
+        arduino = ser.Serial("COM" + com, 9600)
+    except ser.SerialException as e:
+        print(f"Error: {e}")
+        return
+
     while True:
         line = arduino.readline()
         line = line.decode("utf-8")
         msg = json.loads(line)
+        print(msg)
         q.put(msg)
 
-# creating data
-xs = [i * 0.001 for i in range(1001)]
-ys = [0] * 1001
-ys1 = []
-ys2 = []
-
-def update_data():
-    while dpg.is_dearpygui_running():
-        if not q.empty():
-            data = q.get()
-            temperature = float(data["temp"])
-            ys.append(temperature)
-            ys.pop(0)
-            dpg.set_value("temperature_series", [xs, ys])
-        time.sleep(1)  # Update every second
 
 if __name__ == "__main__":
+    com = input("Inserire il numero della COM: ")
+
     mp.freeze_support()
-    q = mp.Queue()  # create the queue
-    p = mp.Process(target=serial_task, args=(q,))
+    q = mp.Queue()
+    p = mp.Process(target=serial_task, args=(q, com))
     p.start()
 
-    with dpg.window(label="Tutorial"):
-        # create plot
-        with dpg.plot(label="Shaded Plot", tag="shaded_plot_1", width=800, height=500):
-            dpg.add_plot_axis(dpg.mvXAxis, label="x")
-            with dpg.plot_axis(dpg.mvYAxis, label="y"):
-                dpg.add_shade_series(xs, ys1, y2=ys2, label="Shaded Area", fill_color=(0, 0, 255, 100))
-                dpg.add_line_series(xs, ys, label="Function", color=(0, 0, 255, 255), tag="temperature_series")
+    with dpg.window(label="Termostato", tag="main_window"):
+        dpg.set_primary_window("main_window", True)
+        with dpg.plot(label="Temperatura", tag="shaded_plot_temp", width=1200, height=500):
+            x_axis_temp = dpg.add_plot_axis(dpg.mvXAxis, label="x", tag="x_axis_temp")
+            y_axis_temp = dpg.add_plot_axis(dpg.mvYAxis, label="y", tag="y_axis_temp")
+            dpg.add_shade_series(xs, ys1_temp, y2=ys2_temp, label="Shaded Area", tag="shaded_area_temp",
+                                 parent=y_axis_temp)
+            dpg.add_line_series(xs, ys_temp, label="Temperatura", tag="temperature_series", parent=y_axis_temp)
 
-    dpg.create_viewport(title='Custom Title', width=1400, height=600)
+        with dpg.plot(label="Umidità", tag="shaded_plot_humid", width=1200, height=500):
+            x_axis_humid = dpg.add_plot_axis(dpg.mvXAxis, label="x", tag="x_axis_humid")
+            y_axis_humid = dpg.add_plot_axis(dpg.mvYAxis, label="y", tag="y_axis_humid")
+            dpg.add_shade_series(xs, ys1_humid, y2=ys2_humid, label="Shaded Area", tag="shaded_area_humid",
+                                 parent=y_axis_humid)
+            dpg.add_line_series(xs, ys_humid, label="Umidità", tag="humidity_series", parent=y_axis_humid)
+
+        with dpg.window(label="Stato LED", pos=(1300, 200), width=325, height=150):
+            dpg.draw_circle((100, 40), 20, color=(0, 0, 0), fill=(0, 30, 0), tag="led1")
+            dpg.add_text("LED 1", pos=(90, 90), tag="led1_text")
+            dpg.draw_circle((200, 40), 20, color=(0, 0, 0), fill=(30, 0, 0), tag="led2")
+            dpg.add_text("LED 2", pos=(190, 90), tag="led2_text")
+
+    dpg.create_viewport(title='Termostato')
+    dpg.configure_viewport("Termostato", width=MAX_WIDTH, height=MAX_HEIGHT, always_on_top=True)
+    dpg.set_viewport_pos([0, 0])
     dpg.setup_dearpygui()
     dpg.show_viewport()
 
-    # Start the update thread
-    update_thread = threading.Thread(target=update_data)
-    update_thread.start()
+    while dpg.is_dearpygui_running():
+        update_data_callback()
+        dpg.render_dearpygui_frame()
 
-    dpg.start_dearpygui()
     dpg.destroy_context()
+    p.terminate()
     p.join()
